6a03e8127617220a297bf14a3698f68cb2a617a6
IsLPos <titanium.jen@gmail.com>
2025-05-29 18:54:56 +0300

    ui

---
+++ Assets/JewelrySimulator/Scripts/CustomerContrller.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomerContrller : MonoBehaviour
+{
+   [SerializeField] private GameObject _customers;
+
+   [SerializeField] private Transform _spawnPos;
+   [SerializeField] private Transform _centerPos;
+   [SerializeField] private Transform _endPos;
+}
