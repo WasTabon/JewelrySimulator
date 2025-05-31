e962a3d93c50c91118952eab09581368fdd91e92
WasTabon <titanium.jen@gmail.com>
2025-05-31 20:55:36 +0300

    Lava

---
+++ Assets/JewelrySimulator/Scripts/LavaController.cs
@@ -0,0 +1,56 @@
+using DG.Tweening;
+using UnityEngine;
+
+public class LavaController : MonoBehaviour
+{
+    [SerializeField] private Transform _lava;
+
+    private void Update()
+    {
+        if (GameState.Instance.state != State.Lava)
+            return;
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            DetectClick(Input.mousePosition);
+        }
+        
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            DetectClick(Input.GetTouch(0).position);
+        }
+    }
+    
+    private void DetectClick(Vector3 screenPosition)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(screenPosition);
+            RaycastHit hit;
+    
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.CompareTag("GoldRed"))
+                {
+                    HandleLavaMovement();
+                }
+                else if (hit.collider.CompareTag("GoldBlue"))
+                {
+                    HandleLavaMovement();
+                }
+                else if (hit.collider.CompareTag("GoldGreen"))
+                {
+                    HandleLavaMovement();
+                }
+            }
+        }
+
+    private void HandleLavaMovement()
+    {
+        Debug.Log("Start Move");
+        _lava.DOLocalMoveY(0.09f, 1f)
+            .SetEase(Ease.OutQuad)
+            .OnComplete((() =>
+            {
+                Debug.Log("Movement Finsihed");
+            }));
+    }
+}
